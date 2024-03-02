91e78c83724a55c508368d08342fd55d6ab0cef5
vishnu vardhan <vishnurockhunters@gmail.com>
2024-03-03 01:07:11 +0530

    update state bug fixed

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, Alert } from 'react-native'
+import { View, Text, Button, Alert, FlatList, TextInput } from 'react-native'
 import useActionCable from './src/hooks/useActionCable';
 import useChannel from './src/hooks/useChannel';
 
 export default function App() {
   const { actionCable } = useActionCable('ws://10.0.2.2:3000/cable')
   const { subscribe, unsubscribe, send } = useChannel(actionCable)
-  const [data, setData] = useState([])
+  const [data, setData] = useState<any>([])
+  const [message, setMessage] = useState("")
 
   const fetchMessages = async () => {
     try {
       const response = await fetch("http://10.0.2.2:3000/messages");
       const responseJson = await response.json();
 
-      console.log(responseJson, "Response Json")
       setData(responseJson)
 
     }
@@ -29,7 +29,23 @@ export default function App() {
   useEffect(() => {
     fetchMessages()
     subscribe({ channel: 'MessagesChannel' }, {
-      received: (x) => setData(x),
+      received: (x) => {
+        console.log(x, "RECEIVED DATA")
+        const receivedData = x;
+        if (receivedData.action === "delete") {
+          // Assuming receivedData.data is an object with an `id` property
+          const id = receivedData.data.id;
+          setData((currentData: any[]) => currentData.filter((item) => item.id !== id));
+        }
+        else if (receivedData.action === "create") {
+          const receivedMessageData = receivedData.data;
+          setData((currentData: any[]) => [...currentData, receivedMessageData]);
+        }
+        else {
+          console.log("swr", receivedData)
+        }
+
+      },
       initialized: function (): void {
         console.log("INIT")
       },
@@ -38,7 +54,6 @@ export default function App() {
       },
       disconnected: function (): void {
         console.log("disconnected")
-
       }
     })
     return () => {
@@ -46,10 +61,65 @@ export default function App() {
     }
   }, [])
 
+  const renderItem = (item: {
+    id: number;
+    body: string;
+  }) => {
+    return (
+      <View>
+        <Text>{item.id}: {item.body}</Text>
+        <Button title='Delete' color={"red"} onPress={() => deleteMessage(item.id)} />
+      </View>
+    )
+  }
+
+  const deleteMessage = async (id: number) => {
+    try {
+      await fetch(`http://10.0.2.2:3000/messages/${id}`, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+    } catch (error) {
+      Alert.alert("Error deleting", JSON.stringify(error))
+    }
+  }
+
+  const sendMessage = async () => {
+    try {
+      if (!message.trim()) {
+        return
+      }
+      const body = message;
+      await fetch("http://10.0.2.2:3000/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ body })
+      })
+    }
+    catch (error) {
+      Alert.alert("Error sending message", JSON.stringify(error))
+    }
+    finally {
+      setMessage("")
+    }
+  }
   return (
     <View>
-      <Text>{JSON.stringify(data)}</Text>
-      <Button title="Click!" onPress={() => send('click', { time: Date.now() })} />
+      <View>
+        <TextInput placeholder='Enter message...' value={message}
+          onChangeText={(text) => setMessage(text)} />
+        <Button title='Send' onPress={sendMessage} />
+      </View>
+      <View>
+        <FlatList data={data}
+          renderItem={({ item }) => renderItem(item)}
+          keyExtractor={(item) => item.id.toString()}
+          extraData={data} />
+      </View>
     </View>
   );
 }
